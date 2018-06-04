8b6cf74106876d65c30db9d725d2a80c79db1d6c
Naz <naziathabib@yahoo.com>
2018-06-04 19:33:56 -0400

    features factory

--- ZFighterz/src/com/page/factory/FeaturesPagePF.java
+++ ZFighterz/src/com/page/factory/FeaturesPagePF.java
@@ -30,5 +30,55 @@ public class FeaturesPagePF
 	{
 		feature1.click();
 	}
+	
+	@FindBy
+	(xpath = "//*[@id=\'second\']/div/div[1]/div[2]/div/ul/li[1]/a/figure/img")
+	WebElement image1;
+	
+	public void image1Method()
+	{
+		image1.click();
+		
+	}
+	
+	@FindBy
+	(xpath = "//*[@id=\'second\']/div/div[1]/div[2]/div/ul/li[2]/a/figure/img")
+	WebElement image2;
+	
+	public void image2Method()
+	{
+		image2.click();
+		
+	}
+	
+	@FindBy
+	(xpath = "//*[@id=\'second\']/div/div[1]/div[2]/div/ul/li[3]/a/figure/img")
+	WebElement image3;
+	
+	public void image3Method()
+	{
+		image3.click();
+		
+	}
+	
+	
+	@FindBy
+	(xpath = "//*[@id=\'footer\']/div/div/div[2]/ul/li[3]/a")
+	WebElement french;
+	
+	public void frenchMethod()
+	{
+		french.click();
+
+	}
+	
+	@FindBy
+	(xpath = "//*[@id=\"footer\"]/div/div/div[2]/ul/li[4]/a")
+	WebElement chinese;
+	
+	public void chineseMethod()
+	{
+		chinese.click();
+	}
 		
 }

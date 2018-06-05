a39a16d7320a6431943642732c9214394b347815
Gurpreet Singh <gsingh573@gmail.com>
2018-06-05 15:04:02 -0400

    gp 6.1 updated

--- ZFighterz/src/com/test/headerlinks/HeaderLinksTest.java
+++ ZFighterz/src/com/test/headerlinks/HeaderLinksTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.page.factory.FeaturesPagePF;
+import com.page.factory.HomePagePF;
 
 public class HeaderLinksTest 
 {
@@ -24,23 +25,41 @@ public class HeaderLinksTest
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		
-		FeaturesPagePF ob1 = new FeaturesPagePF(driver);
-		ob1.getURL();
+		//FeaturesPagePF ob1 = new FeaturesPagePF(driver);
+		//ob1.getURL();
 	}
 	
 	
 	
 	// TESTS ==================================================
-	@Test
+	@Test(priority = 1)
 	public void homeLinkTest() throws InterruptedException
 	{
 		FeaturesPagePF ob1 = new FeaturesPagePF(driver);
+		driver.get("https://www.freecrm.com/features.html");
 		
 		Thread.sleep(1000);
 		ob1.homeLinkMethod();
 		
 		String url = driver.getCurrentUrl();
 		Assert.assertEquals(url, "https://www.freecrm.com/index.html");
+	}
+	
+	@Test(priority = 5)
+	public void customerLinkTest() throws InterruptedException
+	{
+		HomePagePF ob1 = new HomePagePF(driver);
+		
+		driver.get("https://www.freecrm.com/index.html");
+		
+		Thread.sleep(1000);
+		ob1.customerLinkMethod().click();
+		
+		
+		String url = driver.getCurrentUrl();
+		Assert.assertEquals(url, "https://www.freecrm.com/customers.html");
+		
+		
 	}
 	
 	
